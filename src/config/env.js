5021e1fdcc34600f583b81f84bcf3b0945dcbe98
Kauã Pereira <zkauacodes@hotmail.com>
2022-12-03 08:28:18 -0300

    add: adding default values to envConfigs

--- src/config/env.js
+++ src/config/env.js
@@ -1,16 +1,16 @@
 module.exports = {
   envConfig: {
     server: {
-      host: process.env.SERVER_HOST,
-      port: process.env.SERVER_PORT,
-      serverUrl: process.env.SERVER_URL
+      host: process.env.SERVER_HOST || 'localhost',
+      port: process.env.SERVER_PORT || 8080,
+      serverUrl: process.env.SERVER_URL || 'http://localhost:8080'
     },
     database: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 5432,
+      user: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_DATABASE || 'postgres'
     },
     multerStorage: {
       storageType: process.env.MULTER_STORAGE
